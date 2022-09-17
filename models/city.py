83239a6aa2a045a18baa4f15024d37b74a983ad5
njeveh <njeveh@gmail.com>
2022-09-17 21:58:17 +0300

    ammend spacing to enhance readability

--- models/city.py
+++ models/city.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city model class, contains state ID and name """
     __tablename__ = 'cities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
